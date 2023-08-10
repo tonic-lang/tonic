0818c169463a0b1e6c6fb2056928964d0510a6cf
bdasgupta02 <bdasgupta02@gmail.com>
2023-08-11 01:13:26 +0800

    add test for the for stmt parser

--- tnc/tests/frontend/parser_tests.cpp
+++ tnc/tests/frontend/parser_tests.cpp
@@ -26,9 +26,63 @@ TEST(ParserTests, ManualForStatementDecleration) {
     ASSERT_NE(for_node, nullptr);
 
     ASSERT_EQ(for_node->identifier->statement, "i");
+    ASSERT_EQ(for_node->operation, nullptr);
     ASSERT_EQ(for_node->start->statement, "0");
     ASSERT_EQ(for_node->end->statement, "20");
+    ASSERT_EQ(for_node->id_type, "auto");
 }
 
-// const, constexpr variables or const functions too
-// destructor, constructor
+TEST(ParserTests, RangedForStatementDecleration) {
+    std::string statement = "for o in vec";
+
+    Lexer l(statement, file);
+    std::vector<Token> tokens = l.Tokenize();
+
+    Parser p(tokens, file);
+    std::shared_ptr<Node> node = p.ParseForStatement();
+
+    auto ranged_node = std::dynamic_pointer_cast<RangedLoop>(node);
+    ASSERT_NE(ranged_node, nullptr);
+
+    ASSERT_EQ(ranged_node->identifier->statement, "o");
+    ASSERT_EQ(ranged_node->operation, nullptr);
+    ASSERT_EQ(ranged_node->object->statement, "vec");
+    ASSERT_EQ(ranged_node->id_type, "auto");
+}
+
+TEST(ParserTests, ManualForStatementWithType) {
+    std::string statement = "for i: int in 0..20:";
+
+    Lexer l(statement, file);
+    std::vector<Token> tokens = l.Tokenize();
+
+    Parser p(tokens, file);
+    std::shared_ptr<Node> node = p.ParseForStatement();
+
+    auto for_node = std::dynamic_pointer_cast<ForLoop>(node);
+    ASSERT_NE(for_node, nullptr);
+
+    ASSERT_EQ(for_node->identifier->statement, "i");
+    ASSERT_EQ(for_node->operation, nullptr);
+    ASSERT_EQ(for_node->start->statement, "0");
+    ASSERT_EQ(for_node->end->statement, "20");
+    ASSERT_EQ(for_node->id_type, "int");
+}
+
+TEST(ParserTests, ManualRangedStatementWithType) {
+    std::string statement = "for o: int& in vec";
+
+    Lexer l(statement, file);
+    std::vector<Token> tokens = l.Tokenize();
+
+    Parser p(tokens, file);
+    std::shared_ptr<Node> node = p.ParseForStatement();
+
+    auto ranged_node = std::dynamic_pointer_cast<RangedLoop>(node);
+    ASSERT_NE(ranged_node, nullptr);
+
+    ASSERT_EQ(ranged_node->identifier->statement, "o");
+    ASSERT_EQ(ranged_node->operation, nullptr);
+    ASSERT_EQ(ranged_node->object->statement, "vec");
+    ASSERT_EQ(ranged_node->id_type, "int&");
+}
